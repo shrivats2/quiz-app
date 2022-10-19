b5191cace55d605cc7692d233dbd5adb7924ed76
Shrivats <shrivatskrishna25@gmail.com>
2022-10-19 21:34:31 +0530

    initial commit

---
+++ src/components/Start.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {startQuiz} from "../redux/action/quizAAction";
+
+
+const Start = () => {
+  const dispatch = useDispatch();
+  // const {time} = useSelector(state => state.quizReducer)
+  const [minute, setMinute] = useState(1);
+  const [second, setSecond] = useState(0);
+  const [time, setTime] = useState(60);
+
+  useEffect(()=>{
+    const data=window.localStorage.getItem('Time_For_Quiz');
+    console.log('data',data);
+ },[])
+
+ useEffect(()=>{
+  window.localStorage.setItem('Time_For_Quiz',(time))
+ },[time])
+  const handleQuizStart = () => {
+    dispatch(startQuiz(time));
+  };
+  useEffect(() => {
+    if (minute > 59) {
+      setMinute(1);
+    }
+    if (second > 59) {
+      setSecond(1);
+    }
+  }, [minute, second]);
+  useEffect(() => {
+    console.log(minute);
+    console.log(typeof minute);
+    if (minute !== NaN && second !== NaN) {
+      setTime(minute * 60 + second);
+    }
+  }, [minute, second]);
+  return (
+    <div className="startBox">
+      <div>
+        <div>
+          <h1>Start the Quiz</h1>
+          <p>Good luck!</p>
+          <p>Time:&nbsp;&nbsp;{time}sec</p>
+          <section>
+            <label htmlFor="">
+              <input
+                type="number"
+                className="timeInput"
+                value={minute}
+                onChange={(e) => setMinute(parseInt(e.target.value))}
+              />
+              min
+            </label>
+            <label htmlFor="">
+              <input
+                type="number"
+                className="timeInput"
+                value={second}
+                onChange={(e) => setSecond(parseInt(e.target.value))}
+              />
+              sec
+            </label>
+          </section>
+          <button className="startButton" onClick={handleQuizStart}>
+            START
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
